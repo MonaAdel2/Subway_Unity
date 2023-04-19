403ec4ec824078850381d5babdbd5d71538be5bb
omarAlkaley <68498200+omarAlkaley@users.noreply.github.com>
2023-04-19 04:35:50 +0200

    added new enemies and obs prefabs

--- Assets/scripts/coinRotation.cs
+++ Assets/scripts/coinRotation.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class coinRotation : MonoBehaviour
 {
+    [SerializeField] float speed = 45f;
     void Update()
     {
-        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);
+        transform.Rotate(new Vector3(speed, speed , speed) * Time.deltaTime);
     }
 }
