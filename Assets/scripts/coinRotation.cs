9e2b6260ffa4aff207c06e3892f3a071af04bf95
omarAlkaley <68498200+omarAlkaley@users.noreply.github.com>
2023-04-22 07:22:25 +0200

    Added some animations to the buttons and the UI

--- Assets/scripts/coinRotation.cs
+++ Assets/scripts/coinRotation.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class coinRotation : MonoBehaviour
 {
     [SerializeField] float speed = 45f;
+    [SerializeField] bool rotateAroundZOnly;
+
     void Update()
     {
-        transform.Rotate(new Vector3(speed, speed , speed) * Time.deltaTime);
+        if (rotateAroundZOnly)
+        {
+            transform.Rotate(new Vector3(0 , 0 , speed) * Time.deltaTime);
+        }
+        else
+        {
+            transform.Rotate(new Vector3(speed , speed , speed) * Time.deltaTime);
+        }
     }
 }
